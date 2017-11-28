474d85dff6dc070ff472d11714c7376c3209db4c
ReidReininger <34048321+ReidReininger@users.noreply.github.com>
2017-11-27 22:53:35 -0800

    Abstract class GameObject
    ***IMPORTANT*** All game objects must inherit from GameObject. Note that update is a pure virtual function that you must override for derived objects.

---
+++ GameObject.h
@@ -0,0 +1,42 @@
+/*******************************************************************************************
+Programmer: Reid Reininger (charles.reininger@wsu.edu)
+Class: CptS 122, Fall  2017; Lab Section 11
+Programming Assignment: PA9_Hacker_Man
+Date Started: November 27, 2017
+Last Update: November 27, 2017
+
+file: GameObject.h
+
+Description: abstract class from which all game objects must derive
+
+status: functional
+*******************************************************************************************/
+#include <SFML/Graphics.hpp>
+
+using std::string;
+using std::map;
+
+class GameObject {
+public:
+	GameObject();
+	~GameObject();
+
+	virtual void load();
+	virtual void draw(sf::RenderWindow window);
+	virtual void update(float timeLastUpdate, sf::Event event, map<string, GameObject*> gameObjects) = 0;
+
+	virtual void setPosition(float x, float y);
+	virtual void setPosition(sf::Vector2f newPosition);
+	virtual sf::Vector2f getPosition() const;
+	virtual bool isLoaded() const;
+
+	virtual float getWidth() const;
+	virtual float getHeight() const;
+	virtual sf::Rect<float> getBoundingRect() const;
+
+private:
+	sf::Sprite mSprite;
+	sf::Texture mImage;
+	string mFileName; //path to sprite image
+	bool mIsLoaded; //is image loaded
+};
