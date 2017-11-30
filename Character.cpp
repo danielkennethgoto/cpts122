ecd2a838c0e2b446e2a524fc99f566abaea1c736
Reid Reininger <charles.reininger@wsu.edu>
2017-11-30 13:03:06 -0800

    platform collisions

--- Character.cpp
+++ Character.cpp
@@ -3,16 +3,20 @@
 Character::Character()
 	: GameObject::GameObject("stickman.png"),
 	jump{ false },
-	isJumping{false},
-	timeSinceJumpStart{ 0 },
-	velocityX {0},
-	deltaY {0}
+	deltaY{ 0 },
+	velocityX{ 0 },
+	vy0{0},
+	vy{0},
+	freeFallTime{0}
+
 {
 	load();
 	getSprite().setOrigin(getSprite().getGlobalBounds().width / 2, getSprite().getGlobalBounds().height / 2);
 }
 
 void Character::update(float timeLastUpdate, sf::Event event, map<string, GameObject*> gameObjects) {
+	GameObject* platform = gameObjects.find("platform")->second;
+	
 	if (sf::Event::KeyPressed == event.type)
 	{
 		if (sf::Keyboard::Space == event.key.code)
@@ -48,28 +52,43 @@ void Character::update(float timeLastUpdate, sf::Event event, map<string, GameOb
 	
 	if (jump)
 	{
-		isJumping = true;
+		vy0 = jumpVelocity;
 	}
 
-	if (isJumping)
-	{
-		timeSinceJumpStart += timeLastUpdate;
-		deltaY = 0.5 * jumpVelocity * timeSinceJumpStart + 0.5 * acceleration * timeSinceJumpStart * timeSinceJumpStart;
-	}
+	//update freefall
+	freeFallTime += timeLastUpdate;
+	deltaY = 0.5 * vy0 * freeFallTime + 0.5 * g * freeFallTime * freeFallTime;
 
-	if (getSprite().getPosition().y > GameEngine::GROUND - getHeight() / 2 - 1)
+	//if on ground
+	if (getPosition().y > GameEngine::GROUND - getHeight() / 2 - 1)
 	{
-		isJumping = false;
-		getSprite().setPosition(getSprite().getPosition().x, GameEngine::GROUND - getHeight() / 2 - 1);
-		timeSinceJumpStart = 0;
-		velocityY = 0;
+		setPosition(getPosition().x, GameEngine::GROUND - getHeight() / 2 - 1);
+		freeFallTime = 0;
+		vy0 = 0;
 		deltaY = 0;
 	}
 
-	if (getBoundingRect().intersects(gameObjects.find("platform")->second->getBoundingRect()))
+	//if jumping onto platform
+	if (getBoundingRect().intersects(platform->getBoundingRect()))
 	{
-		isJumping = false;
-		getSprite().setPosition(getSprite().getPosition().x, gameObjects.find("platform")->second->getBoundingRect().top - getHeight()/2);
+		if (getPosition().y + getHeight() / 2 < platform->getBoundingRect().top + platform->getHeight())
+		{
+			setPosition(getPosition().x, platform->getBoundingRect().top - getHeight() / 2);
+			freeFallTime = 0;
+			vy0 = 0;
+			deltaY = 0;
+		}
+		else
+		{
+			if (velocityX > 0 && getPosition().x < platform->getPosition().x)
+			{
+				setPosition(platform->getPosition().x - platform->getWidth() / 2 - getWidth() / 2, getPosition().y);
+			}
+			if (velocityX < 0 && getPosition().x > platform->getPosition().x)
+			{
+				setPosition(platform->getPosition().x + platform->getWidth() / 2 + getWidth() / 2, getPosition().y);
+			}
+		}
 	}
 
 	getSprite().move(velocityX * timeLastUpdate, deltaY);
--- Character.h
+++ Character.h
@@ -9,14 +9,23 @@ public:
 	void update(float timeLastUpdate, sf::Event event, map<string, GameObject*> gameObjects);
 
 private:
-	float timeSinceJumpStart;
+	//float timeSinceJumpStart;
 	bool jump;
-	bool isJumping;
-	float velocityY;
-	float deltaY;
-	const float jumpVelocity = -20;
-	const float acceleration = 50;
+	//bool isJumping;
+	//float velocityY;
+	//float deltaY;
+	//const float jumpVelocity = -20;
+	//const float acceleration = 50;
 
 	float velocityX;
 	const float maxVelocityX = 300;
+
+	//new system
+	const float g = 50; //acceleration due to gravity
+	float vy0; //vertical velocity before free fall
+	float vy;
+	float deltaY;
+	float freeFallTime;
+
+	const float jumpVelocity = -20;
 };
